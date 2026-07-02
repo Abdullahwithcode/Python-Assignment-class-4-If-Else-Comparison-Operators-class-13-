4d1c638dd9bab3eb7f6597c9f35a16d036e1691d
Abdullahwithcode <abdullahshabbirmalik20@gmail.com>
2026-07-02 12:04:13 +0500

    Add files via upload
    done

---
+++ . Remove element at index 2, then add 2 new values.py
@@ -0,0 +1,8 @@
+my_list = [10, 20, 30, 40, 50]
+
+del my_list[2]          # removes value at index 2 (30)
+print(my_list)          # [10, 20, 40, 50]
+
+my_list.append(60)
+my_list.append(70)
+print(my_list)          # [10, 20, 40, 50, 60, 70]
---
+++ . Remove first and last element using a method.py
@@ -0,0 +1,6 @@
+my_list = [1, 2, 3, 4, 5]
+
+my_list.pop(0)     # removes first element
+my_list.pop(-1)    # removes last element (pop() also works without arg)
+
+print(my_list)     # [2, 3, 4]
